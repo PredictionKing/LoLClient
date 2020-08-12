91951cc73a65d009eac1168b1800d007ff8ff33d
PredictionKing <bouzztwix@gmail.com>
2020-08-12 19:32:45 +0200

    init commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import com.stirante.lolclient.ClientApi;
+import com.stirante.lolclient.ClientConnectionListener;
+import generated.LolChatUserResource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Initialize API
+        ClientApi api = new ClientApi();
+        //Add listener, which will notify us about client connection available
+        api.addClientConnectionListener(new ClientConnectionListener() {
+            @Override
+            public void onClientConnected() {
+                try {
+                    //Get current user chat info
+                    LolChatUserResource user = api.executeGet("/lol-chat/v1/me", LolChatUserResource.class);
+                    //Print status message
+                    System.out.println(user.statusMessage);
+                    api.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onClientDisconnected() {
+
+            }
+        });
+    }
+}
